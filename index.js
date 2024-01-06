b760ae19c5e5d997b3e9b549e70d7896f1aac2d1
Shivam Tiwari <st472438@gmail.com>
2024-01-06 22:13:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path =require('path');
+const qrcode = require('qrcode');
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'view'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.post('/scan',(req,res)=>{
+    const inp_txt = req.body.text;
+    qrcode.toDataURL(inp_txt,(err,src)=>{
+        res.render('scan',{
+            qr_code:src,
+        });
+    })
+})
+app.listen(3000,console.log(`Listening on port 3000`));
